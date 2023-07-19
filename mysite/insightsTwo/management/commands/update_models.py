389d5a5abef3475a6a418e2cae896853d8559cad
Paul <ramoneblack@me.com>
2023-07-19 07:12:15 -0400

    insightsIndustryUpdate

--- mysite/insightsTwo/management/commands/update_models.py
+++ mysite/insightsTwo/management/commands/update_models.py
@@ -19,6 +19,45 @@ class Command(BaseCommand):
             'healthcare_sect', 'industrial_sect', 'info_technology', 'materials', 
             'real_estate_sect', 'utilities', 'sp_500',]
         
+        sector_dict = {'SP500 Communication Serv Mean': 3, 'Consumer Staples Mean': 4, 'Consumer Disc Mean': 2,
+                'SP500 Energy Mean': 1, 'Finance Sector Mean': 5, 'SP500 Health Care Mean': 6,
+                'Industrial Sector Mean': 7, 'SP500 IT Mean': 8, 'SP500 Materials Mean': 9,
+                'SP500 Real Estate Mean': 10, 'SP500 Utilities Mean': 11, 'S&P 500 Mean':13,}
+        
+        industry_names = [
+            'Aerospace and Defense','Airlines','Auto Components', 'Automobile', 'Bank Industry', 'Beverages', 
+            'Biotechnology', 'Building Products', 'Capital Markets', 'Chemicals', 'Commercial Servives & Supplies', 
+            'Communication Equipment', 'Construction & Engineering', 'Construction Materials', 'Consumer Finance', 
+            'Containers and Packaging', 'Diversified Telecom', 'Diversified Utilities', 'Electrical Equipment', 
+            'Electronic Equipment and Instrument Components', 'Energy Equipment Services', 'Entertainment', 
+            'Equity Real Estate', 'Food Products', 'Food Staples', 'Freight & Logistics', 'Healthcare Equipment Supplies', 
+            'Healthcare Provider Services', 'Hotels Restaurants and Leisure', 'Household Durables', 'Household Products', 
+            'Industrial Conglomerates', 'Information Technology Services', 'Insurance', 'Interactive Media', 'Internet Retail', 
+            'Life Science Tools and Services', 'Machinery', 'Media', 'Metals and Mining', 'Multiline Retail', 
+            'Oil Gas Consumable Fuels', 'Pharmaceuticals', 'Professional Services', 'Regulated Electric', 
+            'Regulated Gas', 'Road and Rail', 'Semiconductors and Semiconductor Equipment', 'Software', 'Solar', 
+            'Specialty Retail', 'Textiles, Apparel, and Luxury Goods', 'Tobacco', 'Trading Companies & Distributors'
+            ]
+
+        industry_dict = {
+            'Aerospace and Defense': 29, 'Airlines': 31, 'Auto Components': 55, 'Automobile': 56, 
+            'Bank Industry': 20, 'Beverages': 57, 'Biotechnology': 24, 'Building Products': 32, 
+            'Capital Markets': 21, 'Chemicals': 47, 'Commercial Servives & Supplies': 33, 
+            'Communication Equipment': 41, 'Construction & Engineering': 34, 'Construction Materials': 48, 
+            'Consumer Finance': 22, 'Containers and Packaging': 49, 'Diversified Telecom': 58, 
+            'Diversified Utilities': 52, 'Electrical Equipment': 35, 'Electronic Equipment and Instrument Components': 42, 
+            'Energy Equipment Services': 18, 'Entertainment': 59, 'Equity Real Estate': 51, 'Food Products': 60, 
+            'Food Staples': 61, 'Freight & Logistics': 30, 'Healthcare Equipment Supplies': 25, 
+            'Healthcare Provider Services': 26, 'Hotels Restaurants and Leisure': 62, 'Household Durables': 63, 
+            'Household Products': 64, 'Industrial Conglomerates': 36, 'Information Technology Services': 43,
+            'Insurance': 23, 'Interactive Media': 65, 'Internet Retail': 14, 'Life Science Tools and Services': 27, 
+            'Machinery': 37,'Media': 66, 'Metals and Mining': 50, 'Multiline Retail': 15, 'Oil Gas Consumable Fuels': 19, 
+            'Pharmaceuticals': 28, 'Professional Services': 38, 'Regulated Electric': 53, 'Regulated Gas': 54, 
+            'Road and Rail': 39, 'Semiconductors and Semiconductor Equipment': 44, 'Software': 45, 'Solar': 46, 
+            'Specialty Retail': 16, 'Textiles, Apparel, and Luxury Goods': 17, 'Tobacco': 67, 
+            'Trading Companies & Distributors': 40
+            }
+        
         
         stats_list = []
         try:
@@ -36,91 +75,88 @@ class Command(BaseCommand):
         except Exception as e:
             print(e)
 
-        sector_dict = {'SP500 Communication Serv Mean': 3, 'Consumer Staples Mean': 4, 'Consumer Disc Mean': 2,
-                   'SP500 Energy Mean': 1, 'Finance Sector Mean': 5, 'SP500 Health Care Mean': 6,
-                   'Industrial Sector Mean': 7, 'SP500 IT Mean': 8, 'SP500 Materials Mean': 9,
-                   'SP500 Real Estate Mean': 10, 'SP500 Utilities Mean': 11, 'S&P 500 Mean':13,}
+      
 
-        sec_name = SectorName.objects.all()
-        weekly_stats = serializers.serialize('json', WeeklyStats.objects.all())
-        get_weekly_stats = json.loads(weekly_stats)
+        # sec_name = SectorName.objects.all()
+        # weekly_stats = serializers.serialize('json', WeeklyStats.objects.all())
+        # get_weekly_stats = json.loads(weekly_stats)
         
-        db_sector_name = set()
-        new_list_of_dates = list()
-        for gw in get_weekly_stats:
-            if "2023" in gw['fields']['date_created'][:10]:
-                db_sector_name = (str(gw['fields']['sector_name']), str(gw['fields']['date_created'][:10]))
-                new_list_of_dates.append(db_sector_name)
+        # db_sector_name = set()
+        # new_list_of_dates = list()
+        # for gw in get_weekly_stats:
+        #     if "2023" in gw['fields']['date_created'][:10]:
+        #         db_sector_name = (str(gw['fields']['sector_name']), str(gw['fields']['date_created'][:10]))
+        #         new_list_of_dates.append(db_sector_name)
 
-        pks_new_list = list()
-        for i in get_weekly_stats:
-            all_sector_numbers = i['fields']['sector_name']
-            if all_sector_numbers not in pks_new_list:
-                pks_new_list.append(all_sector_numbers)
+        # pks_new_list = list()
+        # for i in get_weekly_stats:
+        #     all_sector_numbers = i['fields']['sector_name']
+        #     if all_sector_numbers not in pks_new_list:
+        #         pks_new_list.append(all_sector_numbers)
 
-        pk_num = 0
-        pk_name =""
+        # pk_num = 0
+        # pk_name =""
         
-        while len(pks_new_list) > 0:
-            for i in pks_new_list:
-                pk_num = i
+        # while len(pks_new_list) > 0:
+        #     for i in pks_new_list:
+        #         pk_num = i
 
-            print('i', pk_num)
-            for v in sector_dict.keys():
-                if sector_dict[v] == pk_num:
-                    pk_name = v
+        #     print('i', pk_num)
+        #     for v in sector_dict.keys():
+        #         if sector_dict[v] == pk_num:
+        #             pk_name = v
             
 
-            target_stat = ()
-            for stats in stats_list:
-                if stats[0] == pk_name:
-                    target_stat = stats
+        #     target_stat = ()
+        #     for stats in stats_list:
+        #         if stats[0] == pk_name:
+        #             target_stat = stats
 
             
-            the_price = target_stat[1]
-            the_forward_pe = target_stat[2]
-            the_trailing_pe = target_stat[3]
-            the_forward_ps = target_stat[4]
-            the_trailing_ps = target_stat[5]
-            the_mrq_pb = target_stat[6]
-            the_price_to_cash = target_stat[7]
-            the_quick_ratio = target_stat[8]
-            the_current_ratio = target_stat[9]
-            the_total_debt_to_eq = target_stat[10]
-            the_total_debt_to_cap = target_stat[11]
-            the_roe =  target_stat[12]
-            the_roa = target_stat[13]
-            the_roic = target_stat[14]
-            the_bv_mv = target_stat[15]
-            the_ass_turnover = target_stat[16]
-            the_inv_turnover = target_stat[17]
-            the_op_margin = target_stat[18]
-            the_prof_margin = target_stat[19]
-            the_gross_margin = target_stat[20]
-            the_ev_rev = target_stat[21]
-            the_ev_ebitda = target_stat[22]
-            the_date = target_stat[23]
-            print('the_date', the_date[:10])
+        #     the_price = target_stat[1]
+        #     the_forward_pe = target_stat[2]
+        #     the_trailing_pe = target_stat[3]
+        #     the_forward_ps = target_stat[4]
+        #     the_trailing_ps = target_stat[5]
+        #     the_mrq_pb = target_stat[6]
+        #     the_price_to_cash = target_stat[7]
+        #     the_quick_ratio = target_stat[8]
+        #     the_current_ratio = target_stat[9]
+        #     the_total_debt_to_eq = target_stat[10]
+        #     the_total_debt_to_cap = target_stat[11]
+        #     the_roe =  target_stat[12]
+        #     the_roa = target_stat[13]
+        #     the_roic = target_stat[14]
+        #     the_bv_mv = target_stat[15]
+        #     the_ass_turnover = target_stat[16]
+        #     the_inv_turnover = target_stat[17]
+        #     the_op_margin = target_stat[18]
+        #     the_prof_margin = target_stat[19]
+        #     the_gross_margin = target_stat[20]
+        #     the_ev_rev = target_stat[21]
+        #     the_ev_ebitda = target_stat[22]
+        #     the_date = target_stat[23]
+        #     print('the_date', the_date[:10])
 
-            check_for_previous_entry = (str(pk_num), str(the_date[:10]))
-            print('check_for_previous_entry', check_for_previous_entry)
+        #     check_for_previous_entry = (str(pk_num), str(the_date[:10]))
+        #     print('check_for_previous_entry', check_for_previous_entry)
 
-            if check_for_previous_entry in new_list_of_dates:
-                print(f'Need to pass, {pk_name} at {the_date[:10]} already on file')
-                pass
-            else:
-                print(f'Need to Update, {pk_name} for {the_date[:10]} not on file')
-                models = WeeklyStats(sector_name=SectorName.objects.get(pk=pk_num), price=target_stat[1], forward_pe=target_stat[2], 
-                                    trailing_pe=target_stat[3], forward_ps=target_stat[4], trailing_ps=target_stat[5], 
-                                    mrq_pb=target_stat[6], pcf=target_stat[7], quick_ratio=target_stat[8], 
-                                    current_ratio=target_stat[9], total_debt_to_equity=target_stat[10], 
-                                    total_debt_to_capital=target_stat[11], roe=target_stat[12], roa=target_stat[12], 
-                                    roic=target_stat[14], bv_mv=target_stat[15], ass_turnover=target_stat[16], 
-                                    inv_trnover=target_stat[17], op_margin=target_stat[18], prof_margin=target_stat[19], 
-                                    gross_margin=target_stat[20], ev_revenue=target_stat[21], ev_ebitda=target_stat[22], 
-                                    date_created=target_stat[23])
-                models.save()
-            pks_new_list.remove(pk_num)
+        #     if check_for_previous_entry in new_list_of_dates:
+        #         print(f'Need to pass, {pk_name} at {the_date[:10]} already on file')
+        #         pass
+        #     else:
+        #         print(f'Need to Update, {pk_name} for {the_date[:10]} not on file')
+        #         models = WeeklyStats(sector_name=SectorName.objects.get(pk=pk_num), price=target_stat[1], forward_pe=target_stat[2], 
+        #                             trailing_pe=target_stat[3], forward_ps=target_stat[4], trailing_ps=target_stat[5], 
+        #                             mrq_pb=target_stat[6], pcf=target_stat[7], quick_ratio=target_stat[8], 
+        #                             current_ratio=target_stat[9], total_debt_to_equity=target_stat[10], 
+        #                             total_debt_to_capital=target_stat[11], roe=target_stat[12], roa=target_stat[12], 
+        #                             roic=target_stat[14], bv_mv=target_stat[15], ass_turnover=target_stat[16], 
+        #                             inv_trnover=target_stat[17], op_margin=target_stat[18], prof_margin=target_stat[19], 
+        #                             gross_margin=target_stat[20], ev_revenue=target_stat[21], ev_ebitda=target_stat[22], 
+        #                             date_created=target_stat[23])
+        #         models.save()
+        #     pks_new_list.remove(pk_num)
 
        
     
--- mysite/static/js/insights.js
+++ mysite/static/js/insights.js
@@ -1,9 +1,13 @@
 const sectNames = document.querySelectorAll('.sect_names');
 const pictureTitle = document.querySelector('.sector_title');
-const chartTitle = document.querySelector('.yoy_comp_title');
-const chartArea = document.getElementById('yoy_chart');
 const sectorImage = document.getElementById('sector_image');
 
+const indNames = document.querySelectorAll('.ind_names');
+const indPictureTitle = document.querySelector('.industry_title');
+const industryImage = document.getElementById('industry_image');
+
+const chartTitle = document.querySelector('.yoy_comp_title');
+const chartArea = document.getElementById('yoy_chart');
 
 const priceEarnings = document.getElementById('price_earnings_forward');
 const priceSales = document.getElementById('price_sales_forward');
@@ -11,8 +15,15 @@ const priceBook = document.getElementById('price_book_mrq');
 const priceCashFlow = document.getElementById('price_cashflow');
 const dateCreated = document.getElementById('as_of_date');
 
+const indPriceEarnings = document.getElementById('ind_price_earnings_forward');
+const indPriceSales = document.getElementById('ind_price_sales_forward');
+const indPriceBook = document.getElementById('ind_price_book_mrq');
+const indPriceCashFlow = document.getElementById('ind_price_cashflow');
+const indDateCreated = document.getElementById('ind_as_of_date');
+
 var yearAgoPriceEarnings, yearAgoPriceSales, yearAgoPriceBook, yearAgoPriceCashFlow;
 var currentPriceEarnings, currentPriceSales, currentPriceBook, currentPriceCashFlow;
+var indCurrentPriceEarnings, indCurrentPriceSales, indCurrentPriceBook, indCurrentPriceCashFlow;
 
 
 
@@ -29,7 +40,21 @@ const updateTitle = (sector) => {
         pictureTitle.innerHTML = sector;
         chartTitle.innerHTML = sector;
     }
-   
+}
+
+// UPDATES INDUSTRY TITLE ONCE INDUSTRY IS CHOSEN
+const indUpdateTitle = (industry) => {
+    if (industry === undefined) {
+        indNames.forEach(ind => {
+            ind.addEventListener('click', function(){
+                indPictureTitle.innerHTML = ind.innerHTML;
+                //chartTitle.innerHTML = sect.innerHTML;
+            })
+        })
+    } else {
+        indPictureTitle.innerHTML = industry;
+        //chartTitle.innerHTML = sector;
+    }
 }
 
 // SETS DEFAULT STATS AND IMAGE ON PAGE LOAD SO THAT DIVS AREN'T EMPTY
@@ -60,7 +85,6 @@ const setPicChartStats = () => {
     })
 
     the_weekly_y_o_y.filter((tats) => {
-        console.log('tats', tats)
         if(tats['fields']['sector_name'] == 3){
             chartTitle.innerHTML = "Communication Services"
             yearAgoPriceEarnings = tats['fields']['forward_pe']
@@ -70,6 +94,50 @@ const setPicChartStats = () => {
         }
     })
 }
+
+// SETS INDUSTRY DEFAULT STATS AND IMAGE ON PAGE LOAD SO THAT DIVS AREN'T EMPTY
+const indSetPicChartStats = () => {
+    the_weekly_stats.filter((tats) => {
+        console.log('tats', tats )
+        if (tats['fields']['industry_name'] == 29){
+            indPictureTitle.innerHTML = "Aerospace and Defense"
+            indPriceEarnings.innerHTML = tats['fields']['forward_pe'] + 'x'
+            indPriceSales.innerHTML = tats['fields']['forward_ps'] + 'x'
+            indPriceBook.innerHTML = tats['fields']['mrq_pb'] + 'x'
+            indPriceCashFlow.innerHTML = tats['fields']['pcf'] + 'x'
+
+            let getDate = tats['fields']['date_created'].split('T')
+            let splitDate = getDate[0].split('-')
+            indDateCreated.innerHTML = `${splitDate[1]}-${splitDate[2]}-${splitDate[0]}`
+
+           indCurrentPriceEarnings = tats['fields']['forward_pe']
+           indCurrentPriceSales = tats['fields']['forward_ps']
+           indCurrentPriceBook = tats['fields']['mrq_pb']
+           indCurrentPriceCashFlow =  tats['fields']['pcf']
+        }
+    })
+
+    the_industry_names.filter(image => {
+        // console.log('image', image)
+        if (image['pk'] === 29){
+            console.log('image', image)
+            industryImage.src = 'media/' + image['fields']['industry_image'];
+            console.log('indSetPicChartStats | industryImage.src', industryImage.src)
+        }
+    })
+
+    // the_weekly_y_o_y.filter((tats) => {
+    //     console.log('tats', tats)
+    //     if(tats['fields']['sector_name'] == 3){
+    //         chartTitle.innerHTML = "Communication Services"
+    //         yearAgoPriceEarnings = tats['fields']['forward_pe']
+    //         yearAgoPriceSales = tats['fields']['forward_ps']
+    //         yearAgoPriceBook = tats['fields']['mrq_pb']
+    //         yearAgoPriceCashFlow = tats['fields']['pcf']
+    //     }
+    // })
+}
+
 
 // PROVIDES SECTOR ID | SECTOR ID IS LINKED TO SECTOR NAME
 const getSectorId = () => {
@@ -119,6 +187,180 @@ const getSectorId = () => {
     })
 };
 
+// PROVIDES SECTOR ID | SECTOR ID IS LINKED TO SECTOR NAME
+const getIndustryId = () => {
+    let industryInPlay;
+    indNames.forEach(id => {
+        id.addEventListener("click", function() {
+            let titleID = id.innerHTML
+            switch (titleID){
+                case "Aerospace and Defense":
+                    industryInPlay = 29;
+                    break
+                case "Airlines":
+                    industryInPlay = 31;
+                    break;
+                case "Auto Components":
+                    industryInPlay = 55;
+                    break
+                case "Automobile":
+                    industryInPlay = 56;
+                    break
+                case "Bank Industry":
+                    industryInPlay = 20;
+                    break
+                case "Beverages":
+                    industryInPlay = 57;
+                    break
+                case "Biotechnology":
+                    industryInPlay = 24;
+                    break
+                case "Building Products":
+                    industryInPlay = 32;
+                    break
+                case "Capital Markets":
+                    industryInPlay = 21;
+                    break
+                case "Commercial Services & Supplies":
+                    industryInPlay = 33;
+                    break
+                case "Communication Equipment":
+                    industryInPlay = 41;
+                    break
+                case "Construction & Engineering":
+                    industryInPlay = 34;
+                    break
+                case "Construction Materials":
+                    industryInPlay = 48;
+                    break
+                case "Consumer Finance":
+                    industryInPlay = 22;
+                    break
+                case "Container and Packaging":
+                    industryInPlay = 49;
+                    break
+                case "Diversified Telecom":
+                    industryInPlay = 58;
+                    break
+                case "Diversified Utilities":
+                    industryInPlay = 52;
+                    break
+                case "Electrical Equipment":
+                    industryInPlay = 35;
+                    break
+                case "Electronic Equipment and Instrument Components":
+                    industryInPlay = 42;
+                    break
+                case "Energy Equipment Services":
+                    industryInPlay = 18;
+                    break
+                case "Entertainment":
+                    industryInPlay = 59;
+                    break
+                case "Equity Real Estate":
+                    industryInPlay = 51;
+                    break
+                case "Food Products":
+                    industryInPlay = 60;
+                    break
+                case "Food Staples":
+                    industryInPlay = 61;
+                    break
+                case "Freight & Logistics":
+                    industryInPlay = 30;
+                    break
+                case "Healthcare Equipment Supplies":
+                    industryInPlay = 25;
+                    break
+                case "Healthcare Provider Services":
+                    industryInPlay = 26;
+                    break
+                case "Hotels Restaurants and Leisure":
+                    industryInPlay = 62;
+                    break
+                case "Household Durables":
+                    industryInPlay = 63;
+                    break
+                case "Household Products":
+                    industryInPlay = 64;
+                    break
+                case "Industrial Conglomerates":
+                    industryInPlay = 36;
+                    break
+                case "Information Technology Services":
+                    industryInPlay = 43;
+                    break
+                case "Insurance":
+                    industryInPlay = 23;
+                    break
+                case "Interactive Media":
+                    industryInPlay = 65;
+                    break
+                case "Internet Retail":
+                    industryInPlay = 14;
+                    break
+                case "Life Science Tools and Services":
+                    industryInPlay = 27;
+                    break
+                case "Machinery":
+                    industryInPlay = 37;
+                    break
+                case "Media":
+                    industryInPlay = 66;
+                    break
+                case "Metals and Mining":
+                    industryInPlay = 50;
+                    break
+                case "Multiline Retail":
+                    industryInPlay = 15;
+                    break
+                case "Oil Gas Consumable Fuels":
+                    industryInPlay = 19;
+                    break
+                case "Pharmaceuticals":
+                    industryInPlay = 28;
+                    break
+                case "Professional Services":
+                    industryInPlay = 38;
+                    break
+                case "Regulated Electric":
+                    industryInPlay = 53;
+                    break
+                case "Regulated Gas":
+                    industryInPlay = 54;
+                    break
+                case "Road and Rail":
+                    industryInPlay = 39;
+                    break
+                case "Semiconductors and Semiconductor Equipment":
+                    industryInPlay = 44;
+                    break
+                case "Software":
+                    industryInPlay = 45;
+                    break
+                case "Solar":
+                    industryInPlay = 46;
+                    break
+                case "Specialty Retail":
+                    industryInPlay = 16;
+                    break
+                case "Textiles, Apparel, and Luxury Goods":
+                    industryInPlay = 17;
+                    break
+                case "Tobacco":
+                    industryInPlay = 67;
+                    break
+                case "Trading Companies & Distributors":
+                    industryInPlay = 40;
+                    break
+            }
+            indStatUpdate(industryInPlay);
+            indUpdateImage(industryInPlay);
+            //last_years_stats(industryInPlay)
+        })
+    })
+};
+
 // DISPLAYS SECTOR STATS ON IMAGE WHEN USER SELECTS SECTOR
 const statUpdate = (sectorInPlay) => {
     the_weekly_stats.forEach(stats => {
@@ -148,6 +390,35 @@ const statUpdate = (sectorInPlay) => {
     })
 }
 
+// DISPLAYS INDUSTRY STATS ON IMAGE WHEN USER SELECTS INDUSTRY
+const indStatUpdate = (industryInPlay) => {
+    the_weekly_stats.forEach(stats => {
+        if (stats['fields']['sector_name'] === industryInPlay){
+            indPriceEarnings.innerHTML = stats['fields']['forward_pe'] + 'x'
+            indPriceSales.innerHTML = stats['fields']['forward_ps'] + 'x'
+            indPriceBook.innerHTML = stats['fields']['mrq_pb'] + 'x'
+            indPriceCashFlow.innerHTML = stats['fields']['pcf'] + 'x'
+
+            let getDate = stats['fields']['date_created'].split('T')
+            let splitDate = getDate[0].split('-')
+            indDateCreated.innerHTML = `${splitDate[1]}-${splitDate[2]}-${splitDate[0]}`
+
+            indCurrentPriceEarnings =  stats['fields']['forward_pe']
+            indCurrentPriceSales =  stats['fields']['forward_ps']
+            indCurrentPriceBook = stats['fields']['mrq_pb']
+            indCurrentPriceCashFlow = stats['fields']['pcf']
+        }
+    })
+    // the_weekly_y_o_y.forEach(last => {
+    //     if (last['fields']['sector_name'] === sectorInPlay){
+    //         yearAgoPriceEarnings = last['fields']['forward_pe']
+    //         yearAgoPriceSales = last['fields']['forward_ps']
+    //         yearAgoPriceBook = last['fields']['mrq_pb']
+    //         yearAgoPriceCashFlow = last['fields']['pcf']
+    //     }
+    // })
+}
+
 // UPDATES THE IMAGE THAT IS ASSIGNED TO THE SECTOR NAME
 const updateImage = (sectorInPlay) => {
     the_sector_names.filter(image => {
@@ -162,7 +433,7 @@ const updateImage = (sectorInPlay) => {
 const indUpdateImage = (industryInPlay) => {
     the_industry_names.filter(image => {
         if (image['pk'] === industryInPlay){
-            industryImage.src = 'media/' + image['fields']['sector_image'];
+            industryImage.src = 'media/' + image['fields']['industry_image'];
 
         }
     })
@@ -310,6 +581,12 @@ window.onload = function() {
     
     
     setInterval(carouselStats,  35000);
+
     setPicChartStats()
+    indSetPicChartStats()
+
     updateTitle()
-    getSectorId()    
+    indUpdateTitle()
+
+    getSectorId()
+    getIndustryId()
